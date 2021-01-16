4e50fa6c336cd090d4173a978e6b37326e31c175
Namit Chopra <namitc.11@gmail.com>
2021-01-16 17:06:51 -0500

    changed the script so different content appears with different tabs

---
+++ script.js
@@ -0,0 +1,18 @@
+/*Declaring variables for tab buttons and their conten*/
+const tabs = document.querySelectorAll('.tab-button')
+const allCont = document.querySelectorAll(".tab-content")
+
+tabs.forEach(tab => {
+  tab.addEventListener('click', () => {
+    const num = tab.dataset.forTab;
+    allCont.forEach(cont=>{
+        //console.log(cont.classList.contains("tab-content:active"))
+        if(cont.classList.contains("tab-content:active") == true){
+            cont.classList.remove("tab-content:active");
+            cont.classList.add("tab-content");
+        }
+    })
+    const activate = document.querySelector('.tab-content[data-tab="' + num.toString() + '"]')
+    activate.className += ":active";
+  })
+})
