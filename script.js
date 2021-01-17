ec67a60696a13dc904f1b24b186af8513f3a80e8
Andrew Balmakund <ando20005@gmail.com>
2021-01-16 21:31:15 -0500

    Add new style elements, quotes to home page

--- script.js
+++ script.js
@@ -23,3 +23,19 @@ function setup(){
 }
 
 setup()
+
+//Reference: https://www.freecodecamp.org/news/creating-a-bare-bones-quote-generator-with-javascript-and-html-for-absolute-beginners-5264e1725f08/
+
+var quotes = [
+  "“Mental health…is not a destination, but a process. It’s about how you drive, not where you’re going.” – Noam Shpancer, PhD",
+  "“The strongest people are those who win battles we know nothing about.” - Unknown",
+  "“Don’t let your struggle become your identity.” – Unknown",
+  "“Life doesn’t make any sense without interdependence. We need each other, and the sooner we learn that, the better for us all.”  — Erik Erikson"
+]
+
+console.log("HERE");
+function newQuote(){
+  var randomIndex = Math.floor(Math.random()*(quotes.length));
+  document.getElementById('quoteDisplay').innerHTML = quotes[randomIndex];
+}
+
